3085ba91caabce864a379d80c48034935a3781f0
Elhadj Hocine <hocineelhadj@gmail.com>
2022-04-21 23:01:46 +0100

    Rent Numbers

--- app/src/main/java/com/helic/aminesms/data/models/rent_number/options/RentalOptionsResponse.kt
+++ app/src/main/java/com/helic/aminesms/data/models/rent_number/options/RentalOptionsResponse.kt
@@ -5,6 +5,7 @@ import com.google.gson.annotations.SerializedName
 
 data class RentalOptionsResponse(
     @SerializedName("data")
+    val data: List<RentalOptionsData>,
     val rentalOptionsData: List<RentalOptionsData>,
     @SerializedName("errors")
     val errors: Any,
